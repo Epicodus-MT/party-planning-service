484a374fa98e79c294db131a692b24c18028b0d3
Mara Timberlake <maratimberlake@msn.com>
2017-06-30 09:33:12 -0700

    add .gitignore file

--- src/test/java/PartyPlanningServiceTest.java
+++ src/test/java/PartyPlanningServiceTest.java
@@ -1,11 +1,18 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PartyPlanningServiceTest {
 
   @Test
-  public void runPartyPlanningService_countUpToOne_ArrayList() {
-    // contents of test will eventually go here.
+  public void runPartyPlanningService_setupTrue() {
+    Party testPartyPlanning = new Party (100, "Heavy Apps", "Full Bar", "DJ");
+    assertEquals(true, testPartyPlanning instanceOf Party);
+    testPartyPlanning.
+
+
+
   }
 
 }
